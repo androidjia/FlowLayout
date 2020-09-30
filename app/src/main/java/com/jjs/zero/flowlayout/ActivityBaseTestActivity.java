90e0fed327d8ac51c79188cce49bf469d0e8ef90
junshuai.jia <junshuai.jia@gareatech.com>
2020-09-30 16:07:19 +0800

    无效

--- app/src/main/java/com/jjs/zero/flowlayout/ActivityBaseTestActivity.java
+++ app/src/main/java/com/jjs/zero/flowlayout/ActivityBaseTestActivity.java
@@ -1,7 +1,16 @@
 package com.jjs.zero.flowlayout;
 
+import android.util.SparseArray;
+
 import com.jjs.zero.baseviewlibrary.BaseActivity;
 import com.jjs.zero.flowlayout.databinding.ActivityBaseTestBinding;
+
+import java.util.LinkedHashMap;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 public class ActivityBaseTestActivity extends BaseActivity<ActivityBaseTestBinding> {
 
@@ -13,5 +22,18 @@ public class ActivityBaseTestActivity extends BaseActivity<ActivityBaseTestBindi
     @Override
     protected void initData() {
 
+        SparseArray<String> sparseArray = new SparseArray<>();
+
+//        Observable.just("sdfsdf").flatMap(new Function<String, ObservableSource<?>>() {
+//            @Override
+//            public ObservableSource<?> apply(String string) throws Exception {
+//                return Observable.just("dsf").doOnNext(new Consumer<String>() {
+//                    @Override
+//                    public void accept(String string) throws Exception {
+//
+//                    }
+//                });
+//            }
+//        });
     }
 }
--- app/src/main/java/com/jjs/zero/flowlayout/HttpTestActivity.java
+++ app/src/main/java/com/jjs/zero/flowlayout/HttpTestActivity.java
@@ -15,6 +15,7 @@ import java.util.List;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class HttpTestActivity extends AppCompatActivity {
